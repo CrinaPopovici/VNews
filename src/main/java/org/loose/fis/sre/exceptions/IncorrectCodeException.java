dd4db627c915b4161cf91001b8bea6547d41fa67
carlaploscaru <carla.ploscaru@student.upt.ro>
2022-05-18 13:38:27 +0300

    VNEW-5 logarea administratorului, IncorrectCodeException

---
+++ src/main/java/org/loose/fis/sre/exceptions/IncorrectCodeException.java
@@ -0,0 +1,14 @@
+package org.loose.fis.sre.exceptions;
+
+public class IncorrectCodeException extends Exception{
+    private String username;
+
+    public IncorrectCodeException(String username) {
+        super(String.format("The code for username '%s' is incorrect!", username));
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
